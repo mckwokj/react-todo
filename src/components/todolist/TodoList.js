bcaf0bd971985685c0186aa531e808738182a3ef
Johnson Kwok <kwokjo2@oocl.com>
2021-12-13 19:20:47 +0800

    [TodoList][Johnson Kwok] refactor: remove pass size

--- src/components/todolist/TodoList.js
+++ src/components/todolist/TodoList.js
@@ -3,19 +3,16 @@ import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
 
 const TodoList = () => {
-  const [numberOfTodoItems, setNumberOfTodoItems] = useState(0)
-
   const [itemDetails, setItemDetails] = useState(() => [])
 
   const onGeneratorChange = (itemValue) => {
-    setNumberOfTodoItems(numberOfTodoItems + 1)
     setItemDetails([...itemDetails, itemValue])
   }
 
   return (
     <div>
       <span className="todoListTitle">Todo List</span>
-      <TodoGroup values={itemDetails} size={numberOfTodoItems}/>
+      <TodoGroup values={itemDetails}/>
       <TodoGenerator onGeneratorChange={onGeneratorChange}/>
     </div>
   )
