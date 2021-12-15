a44b9f21c29d7d0d53a19e594fb0a92f1dae2a76
Johnson Kwok <kwokjo2@oocl.com>
2021-12-15 17:59:27 +0800

    [TodoList][Johnson Kwok] feat: rename UPDATE_TODO constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const CREATE_TODO_ITEM = "todo/createTodoItem"
-export const UPDATE_TODO_ITEM_STATUS = "todo/updateTodoItemStatus"
+export const UPDATE_TODO_ITEM = "todo/updateTodoItem"
 export const DELETE_TODO_ITEM_STATUS = "todo/deleteTodoItemStatus"
 export const INIT_TODO = "todo/initTodo"
