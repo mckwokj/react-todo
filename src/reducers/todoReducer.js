66d9e13bad8e3e55a84caa5432f8caea0674ef8a
Johnson Kwok <kwokjo2@oocl.com>
2021-12-15 17:59:40 +0800

    [TodoList][Johnson Kwok] feat: use new UPDATE_TODO constant

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_TODO_ITEM, DELETE_TODO_ITEM_STATUS, INIT_TODO, UPDATE_TODO_ITEM_STATUS } from "../constants/constants";
+import { CREATE_TODO_ITEM, DELETE_TODO_ITEM_STATUS, INIT_TODO, UPDATE_TODO_ITEM } from "../constants/constants";
 
 const initState = {todoItems: []}
 
@@ -7,7 +7,7 @@ const todoReducer = (state = initState, action) => {
     case CREATE_TODO_ITEM:
       return {...state, todoItems: [...state.todoItems, action.payload]}
 
-    case UPDATE_TODO_ITEM_STATUS:
+    case UPDATE_TODO_ITEM:
       const newTodoItems = state.todoItems.map(todo => (todo.id === action.payload.id ? action.payload : todo))
       return {...state, todoItems: newTodoItems} 
 
